35368fc9dc041c7ab41dfb0b4ab2aea012822b38
Gusdizuriadi <gusdizuriadi59826@gmail.com>
2022-12-27 15:26:01 +0700

    tambah tombol kembali di detail artikel

--- resources/views/artikel/detail.blade.php
+++ resources/views/artikel/detail.blade.php
@@ -14,12 +14,13 @@ Detail Artikel
             <div class="row">
                 <div class="col-12 col-sm-6">
                     <div class="col-12">
-                        <img src="{{ url('gambar_artikel/'.$artikel->gambar_artikel) }}" class="product-image" alt="Product Image">
+                        <img src="{{ url('gambar_artikel/'.$artikel->gambar_artikel) }}" class="product-image"
+                            alt="Product Image">
                     </div>
                 </div>
                 <div class="col-12 col-sm-6">
                     <h3 class="my-3">{{ $artikel->judul_artikel }}</h3>
-                    <span class="description"><i class="fas fa-calendar-alt"></i>  {{ $artikel->tanggal_posting }}</span>
+                    <span class="description"><i class="fas fa-calendar-alt"></i> {{ $artikel->tanggal_posting }}</span>
                     <div class="mt-4">
                         <div class="btn btn-info btn-lg btn-flat">
                             <i class="fas fa-tags fa-lg mr-2"></i>
@@ -61,13 +62,20 @@ Detail Artikel
                 </nav>
                 <div class="tab-content p-3" id="nav-tabContent">
                     <div class="tab-pane fade show active" id="product-desc" role="tabpanel"
-                        aria-labelledby="product-desc-tab" style="text-align: justify;"> {{ $artikel->deskripsi_artikel }}
+                        aria-labelledby="product-desc-tab" style="text-align: justify;">
+                        {{ $artikel->deskripsi_artikel }}
                     </div>
+                </div>
+
+                <div class="form-group">
+                    <a href="{{ url('artikel') }}" class=" btn btn-sm btn-secondary"><i
+                            class="fas fa-arrow-circle-left"></i>
+                        Kembali</a>
+                </div>
             </div>
+            <!-- /.card-body -->
         </div>
-        <!-- /.card-body -->
-    </div>
-    <!-- /.card -->
+        <!-- /.card -->
 
 </section>
 
